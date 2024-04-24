0551c9be0a887160b2a975379f8d6d6de9ba1fab
raheem <umar020820@gmail.com>
2024-04-25 04:17:53 +0500

    Pulso

---
+++ src/js/trash.js
@@ -0,0 +1,85 @@
+// const { name } = require("browser-sync");
+
+// var name = "Raheem";
+
+let number = 7;
+const pi = 3.14;
+
+number = 4;
+// pi = 5;
+
+let leftBorderWidth = 200;
+
+// ТИПЫ ДАННЫХ В JS
+
+//number
+//string
+//true/faLse (boolean)
+//null
+// undefind
+// let obj = {
+//     name: 'apple',
+//     color: 'green',
+//     weight: 200
+// }
+//Symbol
+// NaN
+
+// alert(1234)
+
+// console.log(name);
+
+// confirm('Вам есть 18?');
+
+// let answer = confirm('Вам есть 18?');
+// console.log(answer);
+
+// console.log(4+'seesss')
+
+// let  isChecked = false;
+//      isClose = true;
+  
+// console.log(isChecked && isClose); 
+
+// console.log(isChecked || isClose); 
+
+
+// if (2*4 == 8*1) {
+//      console.log('Верно');
+// } else {
+//     console.log('Не верно');
+//  }
+
+// let answer = confirm('Вам есть 18?');
+// if(answer) {
+//     console.log('Пройдите');
+// } else {
+//     console.log('Вход запрещен!');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Неправильно!')
+// } else if(num > 100) {
+//     console.log('Много!')
+// } else {
+//     console.log('Верно!')
+// }
+
+
+
+// for(let i = 1; i < 8; i++) {
+//     console.log(i);
+// }
+
+// for(let i = 8; i > 1; i--) {
+//     console.log(i);
+// }
+
+
+function logging(a, b) {
+    console.log( a + b )
+}
+
+logging(225, 225);
